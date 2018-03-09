64d98423e51ba05b684877481b793d756a55c61e
safpla <haowen.will.xu@gmail.com>
2018-03-09 01:03:54 -0800

    dataset.py

--- dataio/dataset.py
+++ dataio/dataset.py
@@ -1,30 +1,22 @@
+import h5py
 import random
 import tensorflow as tf
 
 class Dataset(object):
-    def __init__(self, data_shape, label_shape, num_classes):
-        self._shape_of_data = data_shape
-        self._shape_of_label = label_shape
-        self._num_classes = num_classes
+    def __init__(self, config):
+        self.config = config
 
-    def load_h5py(self, filename, dset_data='dset_data', dset_label='dset_label'):
+    def load_h5py(self, filename):
         f = h5py.File(filename, "r")
-        self._dataset = f[dset_data]
-        self._labelset = f[dset_label]
-        self._num_examples = self._labelset.len()
+        self._dataset_img = f['img']
+        self._dataset_attrKey = f['attrKey']
+        self._dataset_label = f['label']
+        self._num_examples = self._dataset_label.len()
         print('num of examples: ', self._num_examples)
 
         self._index = np.arange(self._num_examples)
         self._index_in_epoch = 0
         self._epochs_completed = 0
-
-    @property
-    def dataset(self):
-        return self._dataset
-
-    @property
-    def labelset(self):
-        return self._labelset
 
     @property
     def num_examples(self):
@@ -34,8 +26,8 @@ class Dataset(object):
     def epochs_completed(self):
         return self._epochs_completed
 
-    def next_batch(self, batch_size, sparse=False, shuffle=True):
-        # batch_size was thought as the first dimension
+    def next_batch(self, batch_size, shuffle=True):
+        # batch_size is in the first dimension
         start = self._index_in_epoch
         self._index_in_epoch += batch_size
         if self._index_in_epoch > self._num_examples:
@@ -51,17 +43,21 @@ class Dataset(object):
         end = self._index_in_epoch
         batch_index = self._index[start:end]
         batch_index = list(np.sort(batch_index))
-        label = self._labelset[batch_index]
-        data = self._dataset[batch_index]
+        label = self._dataset_label[batch_index]
+        img = self._dataset_img[batch_index]
+        attrKey = self._dataset_attrKey[batch_index]
 
-        if sparse:
-            label_sparse = np.zeros([batch_size, self._num_classes])
-            for i in range(batch_size):
-                label_sparse[i][label[i]] = 1
-            return data, label_sparse
-        else:
-            return data, label
+        return img, label, attrKey
 
     def shuffle(self):
         np.random.shuffle(self._index)
         return
+
+if __name__ == '__main__':
+    filename = '/home/remote/Data/fashionAI/train.hdf5'
+    dataset = Dataset(1)
+    dataset.load_h5py(filename)
+    img, label, attrKey = dataset.next_batch(1)
+    print(label)
+    print(attrKey)
+    print(img.shape)
